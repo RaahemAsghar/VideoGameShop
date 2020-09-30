8dea80cffb559113f25a2909e5c376569b095773
Muhammad Sameer Nadeem <gameshopbyarsh@gmail.com>
2020-09-30 13:01:10 +0500

    Starter Code

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 const path = require('path')
-app.use(express.json({extended: false}))
+const session = require('express-session')
+const flash = require('connect-flash')
+const { runInNewContext } = require('vm')
+
+app.use(express.json({extended: true}))
+app.use(express.urlencoded({extended:false}))
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+   secret:'hello',
+   saveUninitialized:false,
+   resave:true
+}))
+
+app.use(flash())
 
 app.set("view engine", "ejs")
 
@@ -16,13 +29,29 @@ app.get("/table", (req,res)=>{
  })
  
  app.get("/login", (req,res)=>{
-    res.render("login")
+
+   res.render("login",{msg:req.flash('error')})
  })
+
+ app.post('/login', (req,res)=>{
+   const {username, password} = req.body
+   req.flash('error','Credentials are incorrect.')
+
+   res.redirect('/login')
+
+
+ })
+
  app.get("/register", (req,res)=>{
     res.render("register")
  })
  app.get("/profile", (req,res)=>{
     res.render("profile")
+ })
+
+ app.get('/test', (req, res)=>{
+    req.flash('info', "Welcome")
+    res.send(res.locals.flash)
  })
 
  app.get('*', (req,res)=>{
