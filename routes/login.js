6b5acec90962d64700a6de25817be7767caa0726
Haseeb Abid <haseebabid@hotmail.com>
2020-11-18 03:12:19 +0500

    "login backend"

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,48 @@
 const express = require("express");
-
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const getDatabase = require("../db/db").getDatabase;
+
+//const router = express.Router();
+const db = getDatabase();
 
 router.get("/", (req, res) => {
-  res.render("login", { msg: req.flash("error") });
+  res.render("login", { msg: req.flash("login_msg") });
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { username, password } = req.body;
 
-  // make query to database and authorize user
-  // make sure to use bcryptjs
+  const error = [];
+  
+
+  const password_check = `SELECT * FROM user WHERE email = '${username}'`;
+  const [rows, fields] = await db.promise().query(password_check);
+  //console.log(rows);
+  const queried_pass = rows[0].password;
+  //console.log(queried_pass);
+  if (rows.length > 0) {
+  const pass_cmpare = bcrypt.compare(password,queried_pass);
+
+  if(pass_cmpare)
+  {
+    res.redirect("/index");
+
+  }else{
+    errors.push(
+      "Your password does not match your Email"
+    );
+    req.flash("login_msg", errors);
+    res.redirect("/login");
+  }
+}else{
+  errors.push(
+    "There is no user with this email, You might want to create an account"
+  );
+  req.flash("login_msg", errors);
+  res.redirect("/login");
+}
+
 
   res.end();
 });
